7fe577886f5d9282467089b62e2508ebc1de4d0e
ervina1598 <55772923+ervina1598@users.noreply.github.com>
2019-09-24 21:58:03 -0400

    Add files via upload

---
+++ P2T1.SalesPrediction.ErvinAndre.py
@@ -0,0 +1,12 @@
+#Sales Prediction.
+#09/23/2019.
+#CTI-110 P2T1 - Sales Prediction.
+#Andre Ervin.
+#
+total_sales = float(input('Enter the projected sales:'))
+
+#Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+#Display the profit.
+print('The profit is $', format(profit, ',.2f'))
