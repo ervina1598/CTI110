551bb0889237566c5c201248d931325ba39bbf49
ervina1598 <55772923+ervina1598@users.noreply.github.com>
2019-10-06 19:50:11 -0400

    Add files via upload

---
+++ P3HW1_MonthNum_AndreErvin.py
@@ -0,0 +1,22 @@
+#CTI-110
+#P3HW1- Month number
+#Andre Ervin
+#10/04/2019
+#Pseudocode (detail algorithm)
+#
+
+
+#Program to predict the months by numbers.
+months = int(input('Enter number of month:'))
+January = 1
+February = 2
+March = 3
+April = 4
+May = 5
+June = 6
+July = 7
+August = 8
+September = 9
+October = 10
+November = 11
+December = 12
---
+++ P3LAB_Debugging_AndreErvin.py
@@ -0,0 +1,20 @@
+#CTI-110
+#P3LAB- Debugging
+#Andre Ervin
+#10/04/2019
+#Program Pseudocode (detail algorithm)
+#
+
+if score >= A_score:
+    print('Your grade is A.')
+else:
+    if score >= B_score:
+    print('Your grade is B.')
+else:
+    if score >= C_score:
+    print('Your grade is C.')
+else:
+    if score >= D_score:
+    print('Your grade is D.')
+else:
+    print('Your grade is F.')
